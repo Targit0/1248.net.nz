5643a3f5a4fb833ec814b36a535c8207c6ff0b98
jimmy1248 <jimmy1248@gmail.com>
2013-12-14 18:57:20 +1300

    First Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    </head>
+  <body>
+  	
+  	
+                
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//bootstrap-3.0.0/code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap-3.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
